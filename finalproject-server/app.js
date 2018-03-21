513743c1aced4e458f5d030f8a14eb01e55f87aa
YuhanL1n <yuhan.lin1994@gmail.com>
2018-03-21 02:30:48 -0600

    add database helper functions

--- finalproject-server/app.js
+++ finalproject-server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var dbhelper = require('./dbhelper.js');
 
 // Create our app
 var app = express();
@@ -7,6 +8,13 @@ var app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//dbhelper.init();
+
+var comments= dbhelper.getComments('123',function(comments){
+    console.log(comments);
+});
+
 
 app.post('/message', function (req, res) {
     //console.log(req);
---
+++ finalproject-server/dbhelper.js
@@ -0,0 +1,58 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var dbName = 'YouBarrageTube';
+var collectionName = "comments";
+exports.init = function () {
+
+    MongoClient.connect(url + dbName, function (err, db) {
+        if (err) throw err;
+        console.log("Database Created");
+        db.close();
+    });
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db(dbName);
+        dbo.createCollection(collectionName, function (err, res) {
+            if (err) throw err;
+            console.log("Collections Created");
+            db.close();
+        });
+    });
+};
+
+exports.insertComment = function (videoUrl, comment, date) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db(dbName);
+        var query = {_id: videoUrl};
+        var newvalues = {$push: {comments: {comment: comment, date: date}}};
+        var options = {upsert: true};
+        dbo.collection(collectionName).updateOne(query, newvalues, options, function (err, res) {
+            if (err) throw err;
+            console.log(res);
+            db.close();
+        });
+    })
+};
+
+exports.getComments = function (videoUrl,fn) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db(dbName);
+        var query = {_id: videoUrl};
+        var options = {'_id': false};
+        dbo.collection(collectionName).find(query).project(options).toArray(function (err, result) {
+            if (err) throw err;
+            if (result.length){
+                var comments = result[0].comments;
+                comments.sort(function (a,b) {
+                    return parseFloat(a)-parseFloat(b);//TODO: change to date parsing
+                });
+
+            }
+            else comments = [];
+            db.close();
+            fn(comments);
+        });
+    });
+};
