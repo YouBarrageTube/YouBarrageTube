6e6fc99f6deecb6031da4accfa39555e8e9a7781
ZhenguoChen <Zhenguo.Chen@colorado.edu>
2018-04-23 22:30:54 -0600

    add title

--- client/src/components/PlayerContainer.vue
+++ client/src/components/PlayerContainer.vue
@@ -1,28 +1,27 @@
 <template>
-    <div class='container'>
-        <barrage-player :videoId="videoId"/>
-    </div>
+  <div class='container'>
+    <barrage-player :videoId="videoId" />
+  </div>
 </template>
 
 <script>
-import BarragePlayer from "./BarragePlayer";
-
-export default {
-  name: "PlayerContainer",
-  components: {
-    "barrage-player": BarragePlayer
-  },
-  props: ["videoId"]
-};
+  import BarragePlayer from "./BarragePlayer";
+  
+  export default {
+    name: "PlayerContainer",
+    components: {
+      "barrage-player": BarragePlayer
+    },
+    props: ["videoId"],
+  };
 </script>
 
-
 <style scoped>
-.container {
-  margin: auto;
-  width: 60vw;
-  height: 50vh;
-  border: 3px solid #e9e9e9;
-  margin-top: 3%;
-}
+  .container {
+    margin: auto;
+    width: 60vw;
+    height: 50vh;
+    border: 3px solid #e9e9e9;
+    margin-top: 3%;
+  }
 </style>
--- client/src/components/VideoPage.vue
+++ client/src/components/VideoPage.vue
@@ -2,6 +2,7 @@
     <div>
         <navigation-menu/>
         <!--go to barrageplayer container later to format-->
+        <p>{{title}}</p>
         <player-container :videoId="$route.params.id"></player-container>
         <related-video-list></related-video-list>
     </div>
@@ -11,18 +12,30 @@
     import PlayerContainer from './PlayerContainer'
     import NavigationMenu from './NavigationMenu'
     import RelatedVideoList from './RelatedVideoList'
-    
+    import axios from 'axios'
+
     export default {
         name: 'VideoPage',
+        data: function() {
+            return {
+                videoId: "",
+                title: ""
+            }
+        },
         components: {
             'navigation-menu': NavigationMenu,
             'player-container': PlayerContainer,
             'related-video-list': RelatedVideoList
+        },
+        mounted: function() {
+            axios.get(`/v1/video/getById?id=${this.$route.params.id}`)
+                .then(response => this.title = response.data.title)
+                .catch(error => console.log(error));
         }
     }
 </script>
 
 
 <style scoped>
-
+    
 </style>
