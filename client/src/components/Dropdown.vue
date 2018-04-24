c5eefcfebd6d6295bbf520cc297b722f9348e65d
ZhenguoChen <Zhenguo.Chen@colorado.edu>
2018-04-24 10:43:38 -0600

    set zindex

--- client/src/components/Dropdown.vue
+++ client/src/components/Dropdown.vue
@@ -121,7 +121,7 @@
         /* background-color: black; */
         min-width: 160px;
         box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-        z-index: 1;
+        z-index: 101;
         right: 0;
     }
     
